f84cb60e4502319cc8ddb8299c759fb9e64350d1
Sarkunan <sarkunan92@gmail.com>
2017-07-02 00:08:49 +0800

    Readded the tutorial apps, damn ._.

---
+++ redux-booklist/src/actions/index.js
@@ -0,0 +1,12 @@
+function selectBook(book) { 
+  // selectBook is an action creator, it needs to return an action - 
+  // an object with a type property
+  
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
+
+}
+
+export { selectBook }
